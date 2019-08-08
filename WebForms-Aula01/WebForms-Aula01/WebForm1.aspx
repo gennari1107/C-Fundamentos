d4bc738b16556175a237eb571ee437abf0923079
rafael <rafaelgennari@hotmail.com>
2019-08-08 11:22:56 -0300

    Aula01 WebForm

---
+++ WebForms-Aula01/WebForms-Aula01/WebForm1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebForms_Aula01.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        Nome
+                    </td>
+                    <td>
+                        <asp:TextBox ID="TextNome" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Sobrenome
+                    </td>
+                    <td>
+                        <asp:TextBox ID="TextSobrenome" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                   
+                    <td>
+                        <asp:Button ID="BtnCanvelar" runat="server" Text="Cancelar" OnClick="BtnCanvelar_Click" />
+                    </td>
+                    <td>
+                        <asp:Button ID="BtnOk" runat="server" Text="OK" OnClick="BtnOk_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Label ID="lblNomeCompleto" runat="server"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForms-Aula01/WebForms-Aula01/WebForm1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms_Aula01
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnCanvelar_Click(object sender, EventArgs e)
+        {
+            TextNome.Text = string.Empty;
+            TextSobrenome.Text = string.Empty;
+            lblNomeCompleto.Text = string.Empty;
+
+        }
+
+        protected void BtnOk_Click(object sender, EventArgs e)
+        {
+            if (ValidarFormulario() == false)
+            {
+          lblNomeCompleto.Text = "Preencher todos os campos do Formulário";
+
+            return;
+            }
+            lblNomeCompleto.Text = ExibirNomeCompleto(TextNome.Text, TextSobrenome.Text);
+
+            LimparCampos();
+        }
+        private void LimparCampos()
+        {
+            TextNome.Text = string.Empty;
+            TextSobrenome.Text = string.Empty;
+        }
+
+        private string ExibirNomeCompleto(string nome, string sobrenome)
+        {
+            string nomeCompleto = $"{nome} {sobrenome}";
+
+            return nomeCompleto;
+        }
+        
+        private bool ValidarFormulario()
+        {
+            bool valido = true;
+            if (TextNome.Text == string.Empty)
+            {
+                valido = false;
+
+            }
+            if (TextSobrenome.Text == string.Empty)
+            {
+                valido = false;
+            }
+            return valido;
+        }
+    }
+}
